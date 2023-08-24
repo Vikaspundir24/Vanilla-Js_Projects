77151db5f26c079201ab9bb991826f63c75ad86f
vikas pundir <vikaspundiruser@gmail.com>
2023-08-25 01:24:24 +0530

    Word cound app

--- Word Count App/app.js
+++ Word Count App/app.js
@@ -1,35 +1,20 @@
-const start = document.getElementById("start");
-start.innerText = "Start Countdown";
-let resetVal = 0;
-var startCount;
 
-start.addEventListener("click", () => {
-  let inputVal = document.getElementById("count").value;
-  if (inputVal && inputVal != 0 && start.innerText == "Start Countdown") {
-    start.innerText = "Stop Countdown"
-    startCount = setInterval(myCounter, 1000);
-    function myCounter() {
-      if (inputVal < 1) {
-        start.innerText = "Start Countdown"
-        document.getElementById("count").value = "";
-        clearInterval(startCount)
-        return alert("Countdow Over");
-      }
-      inputVal = inputVal - 1;
-      console.log(inputVal);
-      document.getElementById("count").value = inputVal;
+const btnCount = document.getElementById("button");
 
+btnCount.addEventListener("click", () => {
+  countingWords();
+});
+
+function countingWords() {
+  let inputStringInitial = document.getElementById("input").value;
+  let inputString = inputStringInitial.replace(/  +/g, ' ').trim();
+  var numWords = 0;
+  for (let i = 0; i < inputString.length; i++) {
+    var currentCharacter = inputString[i];
+    if (currentCharacter == " ") {
+      numWords += 1;
     }
   }
-  else if(start.innerText == "Stop Countdown"){
-    start.innerText = "Reset Countdown"
-    clearInterval(startCount)
-  }
-  else if(start.innerText == "Reset Countdown"){
-    start.innerText = "Start Countdown"
-    document.getElementById("count").value = "";
-  }
-  else{
-    alert("Enter countdown")
-  }
-});
+  numWords = numWords+1;
+  document.getElementById('count').innerText = numWords;
+} 
