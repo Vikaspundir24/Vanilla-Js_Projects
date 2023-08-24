368a85295baf6299e7842efa72ec009d96ca1c92
vikas pundir <vikaspundiruser@gmail.com>
2023-08-24 22:17:46 +0530

    Stopwatch changes and Countdown app

--- Stopwatch/app.js
+++ Stopwatch/app.js
@@ -10,45 +10,45 @@ document.getElementById("hr").innerText = hr;
 var startTimer;
 
 start.addEventListener("click", () => {
-  if (start.innerText == "Start Timer") 
-  {
-    Timer()
+  if (start.innerText == "Start Timer") {
+    Timer();
   } else if (start.innerText == "Stop Timer") {
     myStopFunction();
   } else if (start.innerText == "Reset Timer") {
-    resetTimer() 
+    resetTimer();
   }
 });
 
 function Timer() {
-    startTimer = setInterval(myTimer, 1000);
-    function myTimer() {
-      sec = sec + 1;
-      if (sec > 60) {
-        sec = 0;
-        min = min + 1;
-      } else if (min > 60) {
-        min = 0;
-        hr = hr + 1;
-      }
-      console.log(hr + "," + min + "," + sec);
-      document.getElementById("sec").innerText = sec;
-      document.getElementById("min").innerText = min;
-      document.getElementById("hr").innerText = hr;
+  startTimer = setInterval(myTimer, 1000);
+  function myTimer() {
+    sec = sec + 1;
+    if (sec > 60) {
+      sec = 0;
+      min = min + 1;
+    } else if (min > 60) {
+      min = 0;
+      hr = hr + 1;
     }
-    start.innerText = "Stop Timer";
+    console.log(hr + "," + min + "," + sec);
+    document.getElementById("sec").innerText = sec;
+    document.getElementById("min").innerText = min;
+    document.getElementById("hr").innerText = hr;
+  }
+  start.innerText = "Stop Timer";
 }
 function myStopFunction() {
-    clearInterval(startTimer);
-    start.innerText = "Reset Timer";
-  }
+  clearInterval(startTimer);
+  start.innerText = "Reset Timer";
+}
 function resetTimer() {
-    start.innerText = "Start Timer";
-    hr = 0;
-    min = 0;
-    sec = 0;
-   document.getElementById("sec").innerText = sec;
-   document.getElementById("min").innerText = min;
-   document.getElementById("hr").innerText = hr;
-  }
-  
+  start.innerText = "Start Timer";
+  hr = 0;
+  min = 0;
+  sec = 0;
+  document.getElementById("sec").innerText = sec;
+  document.getElementById("min").innerText = min;
+  document.getElementById("hr").innerText = hr;
+}
+
+
